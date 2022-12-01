f2d77ce78da68205c0c0e90d6bb12a6b874fd638
poopsicles <87488715+poopsicles@users.noreply.github.com>
2022-12-01 21:56:34 +0100

    add UI to main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
 mod solutions;
 
+use std::io::{self, Write};
+
 fn main() {
-    solutions::one::solve("inputs\\one.txt");
+    println!("<All>\t<#1>\n");
+    print!("What day? ");
+    io::stdout().flush().unwrap();
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    match input.to_lowercase().trim() {
+        "all" => {
+            println!("Day #1");
+            solutions::one::solve("inputs\\one.txt");
+            println!();
+        }
+
+        "1" => solutions::one::solve("inputs\\one.txt"),
+
+        _ => println!("Invalid, oops!"),
+    }
 }
